63d4e6b3104c2eb0edb01c2692d982f3299d72a4
15138730370 <1037483576@qq.com>
2018-01-10 07:29:17 +0800

    save to back

--- src/app/swiper-v/swiper-v.body.ts
+++ src/app/swiper-v/swiper-v.body.ts
@@ -10,5 +10,6 @@ export class SwiperBodyDirective {
 
     setHeight(val) {
         this.render.setStyle(this.ele.nativeElement, 'height', val);
+        this.render.setStyle(this.ele.nativeElement, 'animation-duration', '1s');
     }
 }
